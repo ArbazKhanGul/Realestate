cf231e3725ce7ef21070e46d7d47684681731a76
Arbaz Khan <arbazkhangul123@gmail.com>
2022-02-24 14:23:10 +0500

    image

--- .history/pages/_app_20220224142102.js
+++ .history/pages/_app_20220224142102.js
@@ -17,7 +17,7 @@ Router.events.on('routeChangeComplete',()=>{
   return   <>
     <Head>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
-    <link rel="icon" href="/realestate" />   
+    <link rel="icon" href="/reas" />   
     </Head>
     <ChakraProvider>
 
